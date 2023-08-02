2149e790c6a4f3dfb06cc6dae8eb45dbd0ce54ee
Michael <michael@strongcompute.com>
2023-08-02 02:58:52 +0000

    Figure out how to compute projections

--- learning_atiyah/train.py
+++ learning_atiyah/train.py
@@ -5,35 +5,22 @@ import torch.nn as nn
 import torch.optim as optim
 from poly import PolyM
 
-
-# Simple Linear Model
-class SimpleLinearModel(nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super(SimpleLinearModel, self).__init__()
-        self.linear = nn.Linear(input_dim, output_dim)
-
-    def forward(self, x):
-        return self.linear(x)
-
 def train():
-    rank = dist.get_rank() % ( dist.world_size())
-    model = nn.parallel.DistributedDataParallel(SimpleLinearModel(input_dim, output_dim).to(rank), device_ids=[rank])
-    
+    num_epochs = 10 
+    n_points = 4
 
-
-    input_data, target_data = torch.randn(100, input_dim).to(rank), torch.randn(100, output_dim).to(rank)
+    # (x_i, y_i)
+    p = torch.randn(n_points,2)
     
-    criterion, optimizer, num_epochs = nn.MSELoss(), optim.SGD(model.parameters(), lr=0.01), 10000
+    # differences
+    # ps[j,k] = p[j] - p[k]
+    ps = p.unsqueeze(1) -  p.unsqueeze(0)
     
+    # sum of x_ij^2 + y_ij^2
+    M = ps.square().sum(2).sqrt()
+    xs = ps[:,:,0]
 
-    for epoch in range(num_epochs):
-        optimizer.zero_grad()
-        sleep(10)
-        outputs = model(input_data)
-        loss = criterion(outputs, target_data)
-        loss.backward()
-        optimizer.step()
-        print(f"Rank {rank}, Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}")
+    Xi = torch.stack(( (M + xs).sqrt() , (M - xs).sqrt())) 
 
 if __name__ == "__main__":
     train()
